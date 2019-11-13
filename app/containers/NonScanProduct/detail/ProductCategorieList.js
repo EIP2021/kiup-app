54deb3f962770e41bb75621148f07853458b56a3
thomasyvert <thomas.yvert@epitech.eu>
2019-11-13 22:18:35 +0100

    [ADD] new picture

--- app/containers/NonScanProduct/detail/ProductCategorieList.js
+++ app/containers/NonScanProduct/detail/ProductCategorieList.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { NavigateImageButton } from '../../../components/button';
 
 const categorieList = {
-  spice: { image: 'spice', title: 'Épice' },
-  cheese: { image: 'cheese', title: 'Fromage' },
-  fruit: { image: 'fruit', title: 'Fruit' },
-  vegetable: { image: 'vegetable', title: 'Légume' },
-  meat: { image: 'meat', title: 'Viande' },
-  fish: { image: 'fish', title: 'Poisson' },
-  charcuterie: { image: 'charcuterie', title: 'Charcuterie' },
-  poultry: { image: 'poultry', title: 'Volaille' },
-  cereal: { image: 'cereal', title: 'Céréale' },
-  starchy: { image: 'starchy', title: 'Féculent' },
-  alcohol: { image: 'alcohol', title: 'Alcool' },
+  spice: { image: 'spice', title: 'Épice', id: '1005' },
+  herb: { image: 'herb', title: 'Herbe', id: '1006' },
+  cheese: { image: 'cheese', title: 'Fromage', id: '501' },
+  fruit: { image: 'fruit', title: 'Fruit', id: '204' },
+  vegetable: { image: 'vegetable', title: 'Légume', id: '201' },
+  meat: { image: 'meat', title: 'Viande', id: '402' },
+  fish: { image: 'fish', title: 'Poisson', id: '406' },
+  charcuterie: { image: 'charcuterie', title: 'Charcuterie', id: '403' },
+  starchy: { image: 'starchy', title: 'Féculent', id: '301' },
+  bread: { image: 'bread', title: 'Pain', id: '302' },
+  alcohol: { image: 'alcohol', title: 'Alcool', id: '603' },
 };
 
 const ProductCategorieList = () => {
