37257c23125520817140a5fc21d52decda9e3c0b
Johan Lhour <johan.lhour@epitech.eu>
2020-06-01 16:47:34 +0200

    [CLEAN]: clean forgotten data (#25)

--- app/containers/Profile/ProfileScreen.js
+++ app/containers/Profile/ProfileScreen.js
@@ -5,24 +5,9 @@ import PropTypes from 'prop-types';
 import styles from './styles/ProfileScreenStyle';
 import { Header, TitleField } from '../../components';
 import NutrimentsStats from './detail/NutrimentsStats';
-import ConsumptionChartSwiper from './detail/ConsumptionChartSwiper';
 import HistoryList from './detail/HistoryList';
 
-const monthlyStats = {
-  potassium: [320, 430, 210, 560, 0, 320, 730],
-  phosphorus: [320, 430, 210, 560, 0, 320, 730],
-  proteins: [32, 43, 21, 56, 0, 32, 73],
-  salt: [3.2, 4.1, 2.8, 5.1, 0, 3.2, 7.3],
-};
-
 const ProfileScreen = ({ navigation }) => {
-  const ConsumptionCharWeekly = (
-    <ConsumptionChartSwiper nutrimentsStats={monthlyStats} type="weekly" />
-  );
-
-  const ConsumptionCharMonthly = (
-    <ConsumptionChartSwiper nutrimentsStats={monthlyStats} type="monthly" />
-  );
   return (
     <ScrollView nestedScrollEnabled style={styles.container}>
       <Header
@@ -31,10 +16,7 @@ const ProfileScreen = ({ navigation }) => {
         onIconPress={() => navigation.navigate('Setting')}
       />
       <TitleField title="Statistiques" titleStyle={styles.statsTitle} />
-      <NutrimentsStats
-        consumptionCharWeekly={ConsumptionCharWeekly}
-        consumptionCharMonthly={ConsumptionCharMonthly}
-      />
+      <NutrimentsStats />
       <TitleField
         title="Consomations récentes"
         titleStyle={styles.consumeTitle}
